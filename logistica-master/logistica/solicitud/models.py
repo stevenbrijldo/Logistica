265b194fa65967eb2c4005685de689d9e7dcbe4c
stevenbrijldo <stevenscano_92@hotmail.com>
2013-11-19 22:35:30 -0500

    carga trabajo
    carga trabajo

--- logistica-master/logistica/solicitud/models.py
+++ logistica-master/logistica/solicitud/models.py
@@ -3,14 +3,11 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
-<<<<<<< HEAD
-=======
 class Solicitud(models.Model):
-	codigo = models.IntegerField(primary_key=True)
+	codigo = models.AutoField(primary_key=True)
 	cliente = models.IntegerField()
 	descripcion = models.CharField(max_length=100)
 	estado =models.CharField(max_length=100)
 	fecha = models.DateField(null=False)
 	def __unicode__(self):
 		return self.nombre
->>>>>>> 000c2dff42fb7a3918687620a8ad2e97406f0deb
--- logistica-master/logistica/solicitud/views.py
+++ logistica-master/logistica/solicitud/views.py
@@ -4,31 +4,53 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
-<<<<<<< HEAD
-
-from datetime import *
-
-def solicitud(request):
-	return render_to_response ('solicitud.html',{'cont':3}, context_instance=RequestContext(request))
-=======
 from solicitud.models import Solicitud
+from django.core import serializers
 
 def solicitud(request):
-	return render_to_response ('solicitud.html',{'cont':3}, context_instance=RequestContext(request))
+	solicitud=Solicitud.objects.all()
+	return render_to_response('solicitud.html',{'cont':3,'solicitudes':solicitud}, context_instance=RequestContext(request))
 def holaMundo(request):
      html = "<html><body>Hola Mundo desde DJANGO</body></html>"
      return HttpResponse(html)
 def agregarSolicitud(request):
+	if request.method == 'POST':
+		
+		cliente = request.POST['cliente']
+		descripcion = request.POST['descripcion']
+		estado = request.POST['estado']
+		fecha = request.POST['fecha']
+		solicitud = Solicitud.objects.create(cliente=cliente,descripcion=descripcion,estado=estado,fecha=fecha)
+		solicitud=Solicitud.objects.all()
+		return render_to_response('solicitud.html',{'cont':3,'solicitudes':Solicitud.objects.all()}, context_instance=RequestContext(request))
+	else:
+		solicitud=Solicitud.objects.all()
+		return render_to_response('solicitud.html',{'cont':3,'solicitudes':solicitud}, context_instance=RequestContext(request))
+def modificarSolicitud(request):
 	if request.method == 'POST':
 		codigo = request.POST['codigo']
 		cliente = request.POST['cliente']
 		descripcion = request.POST['descripcion']
 		estado = request.POST['estado']
 		fecha = request.POST['fecha']
-		solicitud = Solicitud.objects.create(cliente=cliente,codigo=codigo,descripcion=descripcion,estado=estado,fecha=fecha)
+		solicitud = Solicitud.objects.get(codigo=codigo)
+		solicitud.cliente=cliente
+		solicitud.descripcion=descripcion
+		solicitud.estado=estado
+		solicitud.fecha=fecha
+		solicitud.save()
 		solicitud=Solicitud.objects.all()
-		return render_to_response('solicitud.html',{'cont':3}, context_instance=RequestContext(request))
+		return render_to_response('solicitud.html',{'cont':3,'solicitudes':Solicitud.objects.all()}, context_instance=RequestContext(request))
 	else:
 		solicitud=Solicitud.objects.all()
-		return render_to_response('solicitud.html',{'cont':3}, context_instance=RequestContext(request))
->>>>>>> 000c2dff42fb7a3918687620a8ad2e97406f0deb
+		return render_to_response('solicitud.html',{'cont':3,'solicitudes':solicitud}, context_instance=RequestContext(request))
+def buscarSolicitud(request):
+	if request.isajax():
+		solicitud=Solicitud.objects.all()
+		descripcion = request.POST['descripcion']
+		solicitud=solicitud.filter(descripcion__icontains=descripcion)
+		data=serializers.serialize("json",solicitud)
+		return HttpResponse(data,context_type="aplication/json")
+	else:
+		solicitud=Solicitud.objects.all()
+		return render_to_response('solicitud.html',{'cont':3,'solicitudes':solicitud}, context_instance=RequestContext(request))
